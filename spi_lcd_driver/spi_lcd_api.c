8f5592bc5cd930b8c56ed6112f8160bdc7cc1ae6
flyyingpiggy2020 <154562451@qq.com>
2023-11-26 10:56:29 +0800

    feat:完成spi驱动

---
+++ spi_lcd_driver/spi_lcd_api.c
@@ -0,0 +1,24 @@
+/*
+ * Copyright (c) 2023 by Moorgen Tech. Co, Ltd.
+ * @FilePath     : spi_lcd_api.c
+ * @Author       : lxf
+ * @Date         : 2023-11-25 15:01:48
+ * @LastEditors  : flyyingpiggy2020 154562451@qq.com
+ * @LastEditTime : 2023-11-25 16:14:41
+ * @Brief        : spi lcd init
+ */
+
+/*---------- includes ----------*/
+
+#include "stdint.h"
+/*---------- macro ----------*/
+/*---------- type define ----------*/
+/*---------- variable prototype ----------*/
+/*---------- function prototype ----------*/
+/*---------- variable ----------*/
+/*---------- function ----------*/
+
+void spi_lcd_init(void)
+{
+}
+/*---------- end of file ----------*/
